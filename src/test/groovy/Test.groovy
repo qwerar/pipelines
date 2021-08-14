3892df825eec8ead55de79e8d012f695fba2f473
pomirko_y <y.pomirko@tickets.com.ua>
2021-08-14 14:18:14 +0300

    debug

--- src/test/groovy/Test.groovy
+++ src/test/groovy/Test.groovy
@@ -1,19 +1,25 @@
 class Test {
 
+
+
     static void main(String[] args) {
-        List<Integer> val = Arrays.asList(1, 3, 4, 5)
-        println sum(val, 6)
+       /* List<Integer> val = Arrays.asList(1, 3, 4, 5)
+        println sum(val, 6)*/
 
         // println(val.toArray())
+        firstFuncion("Uo")
     }
 
-    static int sum(List<Integer> list, int condition) {
+/*    static int sum(List<Integer> list, int condition) {
         int sum = 0;
         for (Integer t : list) {
             sum += t;
         }list.parallelStream()
         return sum;
 
+    }*/
+    static def firstFuncion(name){
+        print( "Hello this is ${name}")
     }
 
 
